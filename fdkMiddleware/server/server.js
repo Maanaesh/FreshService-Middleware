9fd5dde1e355185db5b467bf5d6f59b259f11f3c
Maanaesh <maanaeshswamy@gmail.com>
2024-09-24 12:03:03 +0530

    added custom installation page updated middleware logic

--- fdkMiddleware/server/server.js
+++ fdkMiddleware/server/server.js
@@ -5,9 +5,48 @@ exports = {
   // args is a JSON block containing the payload information.
   // args['iparam'] will contain the installation parameter values.
   onTicketCreateHandler: async function(args) {
-    console.log('Hello ' + args['data']['requester']['name']);
-    const response = await axios.get("http://localhost:3000/");
-    console.log(response.data);
+    const jsonObj = {};
+    const selectedFields = args.iparams.selectedFields;
+
+    if (args.data && args.data.ticket) {
+      selectedFields.forEach((field) => {
+        const fieldNameLower = field.name.toLowerCase(); 
+
+        
+        const ticketKeys = Object.keys(args.data.ticket).map(key => key.toLowerCase());
+
+       
+        if (ticketKeys.includes(fieldNameLower)) {
+          jsonObj[fieldNameLower] = args.data.ticket[fieldNameLower];
+        } else {
+          console.log(`Field ${field.name} does not exist in ticket data.`);
+        }
+      });
+    } else {
+      console.error("Invalid selectedFields or ticket data.");
+    }
+
+   
+    jsonObj["email"] = args.data.requester.email;
+
+    const api_key = args.iparams.key;
+    const url = args.iparams.url;
+
+  try {
+    // Making the POST request to create a ticket
+    const response = await axios.post(`http://localhost:3000/api/createTickets/`,{
+      url:url,
+      key:api_key,
+      ticket:jsonObj,
+    });
+    console.log('Success:', response.status);
+
+  } catch (error) {
+  
+    console.log('Error Response:', error.response.status, error.response.data);
+    } 
   }
 
-};
+}
+
+
--- middleware/server.js
+++ middleware/server.js
@@ -1,38 +1,47 @@
 import express from 'express';
-const app = express();
+import dotenv from 'dotenv';
 import { connectDB } from './db.js';
 import Ticket from './ticket.js';
 import handler from './handler.js';
-app.use(express.json());
-const port=3000;
-const url="https://effy-opinyin.freshdesk.com";
-const key="GNqXIlHkv2vmPWhxg6zE";
 
-app.post("/api/createTickets",async (req,res)=>{
-    const ticket=req.body;
-    // const existingTicket = await Ticket.findOne({ ticket_id: ticket.id });
-    const existingTicket=false;
+dotenv.config();  // Load environment variables from .env file
+
+const app = express();
+app.use(express.json());
+const port = 3000;
+const FRESHDESK_KEY='GNqXIlHkv2vmPWhxg6zE';
+const FRESHDESK_URL='https://effy-opinyin.freshdesk.com';
+
+
+// POST request to update key and url
+app.post("/api/createTickets", async (req, res) => {
+  const ticket = req.body.ticket;
+  
+  ticket.pushed_to_freshdesk = false;
+  console.log(ticket);
+
+  // const existingTicket = await Ticket.findOne({ ticket_id: ticket.id });
+  const existingTicket = false;
   if (existingTicket) {
     return res.status(409).json({ message: 'Ticket already exists' });
   }
-  await Ticket.create({
-    subject: ticket.subject,
-    description: ticket.description,
-    status: 2,
-    created_at: ticket.created_at,
-    pushed_to_freshdesk: false,
-    email:ticket.email,
-  });
+
+  await Ticket.create(ticket);
+
+ 
+  // process.env.FRESHDESK_KEY = freshdeskKey;
+  // process.env.FRESHDESK_URL = freshdeskUrl;
+
   res.status(200).json({ message: 'Ticket stored successfully' });
 });
 
-app.get("/",async(req,res)=>{
-    return res.status(200).json({message:"hello!"});
+app.get("/", async (req, res) => {
+  return res.status(200).json({ message: "hello!" });
 });
 
-
-
 app.listen(port, () => {
-    connectDB();
-    console.log(`Middleware listening on port ${port}`)});
-    handler(key,url);
+  connectDB();
+  console.log(`Middleware listening on port ${port}`);
+  
+  handler(FRESHDESK_KEY, FRESHDESK_URL);
+});
